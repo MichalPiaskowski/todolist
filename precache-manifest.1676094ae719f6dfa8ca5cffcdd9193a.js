ea7ea895bc49587d81612a7711dbec00494ab102
MichalP <mich.piaskowski@gmail.com>
2019-04-15 12:44:52 +0200

    Updates

---
+++ precache-manifest.1676094ae719f6dfa8ca5cffcdd9193a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "b4b7f9866387d4efef0c",
+    "url": "/todolist/static/js/main.6514993e.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/todolist/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "b4b7f9866387d4efef0c",
+    "url": "/todolist/static/css/main.3a453d04.chunk.css"
+  },
+  {
+    "revision": "e06edf80a83abcd08af7301cf39d88bc",
+    "url": "/todolist/index.html"
+  }
+];
